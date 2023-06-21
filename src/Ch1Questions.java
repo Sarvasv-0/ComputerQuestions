090f2815156e681426feee6bdb72acc60ab12dd9
Sarvasv-0 <81177923+Sarvasv-0@users.noreply.github.com>
2023-06-21 14:59:29 +0530

    Ch1-Q3 Solved

--- src/Ch1Questions.java
+++ src/Ch1Questions.java
@@ -21,6 +21,7 @@ public class Ch1Questions {
 //        Question 2
 //        Write a Java program to check if a letter input by the user is vowel or consonant
 
+/*
         System.out.print("Enter a letter: ");
         char letter = sc.next().charAt(0);
         if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'){
@@ -29,5 +30,18 @@ public class Ch1Questions {
         else {
             System.out.println("The Input letter is a consonant");
         }
+*/
+
+//        Question 3
+//        Write a program to swap values of two variables without creating third variable
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.println("first number: "+num1+" and second number: "+num2);
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("first number: "+num1+" and second number: "+num2);
     }
 }
