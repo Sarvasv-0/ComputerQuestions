d991f71cbcfe21884a810fecd579a2f9b3770024
Sarvasv-0 <81177923+Sarvasv-0@users.noreply.github.com>
2023-06-21 14:38:33 +0530

    Ch1-Q1 Solved

--- src/Ch1Questions.java
+++ src/Ch1Questions.java
@@ -4,5 +4,18 @@ public class Ch1Questions {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+//        Question 1
+//        Write a program to check if a number input by the user is vowel or consonant
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        if (number%2 == 0){
+            System.out.println("The Input number is even");
+        }
+        else {
+            System.out.println("The Input number is odd");
+        }
+
+
     }
 }
