1224f57cc5a815c97632f2b175349f694d40c494
Sarvasv-0 <81177923+Sarvasv-0@users.noreply.github.com>
2023-07-29 16:26:21 +0530

    Ch2 Q3 solved

--- src/Ch2Questions.java
+++ src/Ch2Questions.java
@@ -12,19 +12,37 @@ public class Ch2Questions {
 //        Question 2
 //        Write a program to subtract a float number from an integer number and get the result in integer by using local variable
 
+/*
         System.out.print("Enter a floating point number: ");
         float x = sc.nextFloat();
         System.out.print("Enter an integer: ");
         int y = sc.nextInt();
         int z = (int) (y-x);
         System.out.println("The Subtraction of x from y is "+z);
+*/
+
+//        Question 3
+//        Write a program to print your name by creating an object
+        Student rollNo39 = new Student();
+        rollNo39.setName("Sarvasv");
+        System.out.println("My name is: "+rollNo39.getName());
+
 
     }
 }
 class Question1 {
-    public static int num1 = 100;
-    public static int num2 = 600;
+    static int num1 = 100;
+    static int num2 = 600;
     public static void add(){
         System.out.println("The Sum of 100 and 600 is: "+ (num1+num2));
+    }
+}
+class Student{
+    String name;
+    public void setName(String x){
+        name = x;
+    }
+    public String getName(){
+        return name;
     }
 }
