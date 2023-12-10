5638a6fad5fa7ca4b4c9538f397e3d6e7240b5f1
Sarvasv-0 <81177923+Sarvasv-0@users.noreply.github.com>
2023-12-10 15:11:48 +0530

    2nd chapter solved (kinda)

--- src/Chapter2Questions/Q6.java
+++ src/Chapter2Questions/Q6.java
@@ -1,7 +1,35 @@
 package Chapter2Questions;
 
 public class Q6 {
-    public static void main(String[] args) {
+    int num1=25;
+    float num2=24.43f;
+    double num3=71.02d;
 
+    public int getNum1() {
+        return num1;
+    }
+
+    public float getNum2() {
+        return num2;
+    }
+
+    public double getNum3() {
+        return num3;
+    }
+    public double calc(double x) {
+        return x*num3;
+    }
+
+    public int sum(int a,int b){
+        return a+b+num1;
+    }
+
+    public static void main(String[] args) {
+        Q6 obj1 = new Q6();
+        System.out.println(obj1.getNum1());
+        System.out.println(obj1.getNum2());
+        System.out.println(obj1.getNum3());
+        System.out.println(obj1.calc(28.2f));
+        System.out.println(obj1.sum(29,38));
     }
 }
